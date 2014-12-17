c8b57df9bdf852284066070171fca10e4f17493f
neo <herbertdawkins@gmail.com>
2014-12-17 12:50:08 -0500

    added information to digraph file

--- modules/digraph.py
+++ modules/digraph.py
@@ -1,3 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        digraph
+# Purpose:
+#
+# Author:      HDizzle
+#
+# Created:     06/Sept/2014
+# Copyright:   (c) HDizzle 2014
+# Licence:     MIT
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
 import pygraphviz as pgv
 import modules.astor as astor
 
